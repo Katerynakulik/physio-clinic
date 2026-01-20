3fa60d4dc8e4fc6a8a019c489d4a502b8332aa8e
Katerynakulik <katerynakulik4@gmail.com>
2026-01-20 12:15:10 +0200

    Register and migrate models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ClientProfile, Physiotherapist
 
-# Register your models here.
+admin.site.register(ClientProfile)
+admin.site.register(Physiotherapist)
