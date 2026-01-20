84a7614c36be59b300bffb711a4fc38964a9428d
Katerynakulik <katerynakulik4@gmail.com>
2026-01-20 12:10:44 +0200

    Create accounts models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ClientProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20, blank=True)
+    insurance_number = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return self.user.email
+
+
+class Physiotherapist(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    specialization = models.CharField(max_length=100)
+    working_from = models.TimeField()
+    working_to = models.TimeField()
+
+    def __str__(self):
+        return self.user.get_full_name()
