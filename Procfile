50ec71d6ae285d86d8a6a11df47d220c4afb84f7
Katerynakulik <katerynakulik4@gmail.com>
2026-01-20 12:58:30 +0200

    Configure buildpack and requirements

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn phisio-clinic.wsgi
--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['physio-clinic.herokuapp.com', '127.0.0.1', 'localhost',]
 
 
 # Application definition
