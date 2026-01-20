50598965dac777cace689f5324530e5414952834
Katerynakulik <katerynakulik4@gmail.com>
2026-01-20 22:28:13 +0200

    Create clean login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import register_client
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('register/', register_client, name='register'),
-    path('dashboard/', views.client_dashboard, name='client_dashboard'),
+    path("register/", views.register_client, name="register"),
+    path("login/", auth_views.LoginView.as_view(
+        template_name="accounts/login.html"
+    ), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("dashboard/", views.client_dashboard, name="client_dashboard"),
 ]
