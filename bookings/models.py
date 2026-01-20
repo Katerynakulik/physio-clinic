d8d9f94ee636c0f9a40ca2bdeaa34e4f9624f203
Katerynakulik <katerynakulik4@gmail.com>
2026-01-20 12:12:08 +0200

    Create booking model

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from accounts.models import ClientProfile, Physiotherapist
 
-# Create your models here.
+class Booking(models.Model):
+    client = models.ForeignKey(ClientProfile, on_delete=models.CASCADE)
+    physiotherapist = models.ForeignKey(Physiotherapist, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_cancelled = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.client} - {self.date} {self.time}"
