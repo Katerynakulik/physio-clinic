1f1bc397910a36be2bfdc5c029dfee33f7f384ca
Katerynakulik <katerynakulik4@gmail.com>
2026-01-20 16:37:56 +0200

    Configure global templates directory and add Bootstrap base template

--- config/settings.py
+++ config/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,6 +73,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
