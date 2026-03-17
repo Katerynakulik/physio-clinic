372ca36c53dc339f52d712d846891017f8c2fe38
Kateryna Kulik <katerynakulik4@gmail.com>
2026-03-17 15:27:18 +0100

    Create 404.html and make chenges according to W3C

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-only-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+# DEBUG = os.environ.get("DEBUG", "False") == "True"
+
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
